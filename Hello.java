f21b0953f4d25e05361fa71be7dab87180f48590
Yasas Priyamantha <78372973+YasasPriyamantha@users.noreply.github.com>
2021-10-05 11:22:34 +0530

    Add files via upload
    New java project - Hello world project

---
+++ Hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hello{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in)
+		String name;
+		int age;
+		
+		System.out.print("Enter your name :")
+		name = scanner.next();
+		System.out.print("Enter your age  :")
+		age = scanner.nextInt();
+		System.out.print("Your name is : "+name)
+		System.out.print("Your age is : "+age)
+
+	}
+}
