efcb6b43eb6bb3a1e6341b8e98593fb91ffdbc81
Yasas Priyamantha <78372973+YasasPriyamantha@users.noreply.github.com>
2021-10-06 08:46:38 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,13 +5,13 @@ public class Hello{
 		Scanner scanner = new Scanner(System.in);
 		String name;
 		int age;
-		
-		System.out.print("Enter your name :");
+		System.out.print("Hellooooooooo000");
+		/*System.out.print("Enter your name :");
 		name = scanner.next();
 		System.out.print("Enter your age  :");
 		age = scanner.nextInt();
 		System.out.print("Your name is : "+name);
-		System.out.print("Your age is : "+age);
+		System.out.print("Your age is : "+age);*/
 
 	}
 }
